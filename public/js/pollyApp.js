9193b25555b4f41f5974e0efb6b7bad8f862a70b
Toan <toan@wontoan.com>
2015-12-08 20:50:39 -0500

    Set default options to 2 and set up boilerplate angular app in pollyApp.js

---
+++ public/js/pollyApp.js
@@ -0,0 +1,10 @@
+(function () {
+  
+  var app = angular.module("pollyApp", []);
+  app.controller('pollController', function () {
+    $scope.newPoll = function(){
+      
+    };
+  });
+  
+})();
